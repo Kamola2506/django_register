f6ba87eeadcb4c647e2f0460f9020b80146f2d58
Kamola2506 <kamolamirzaqulova327@gmail.com>
2026-03-12 22:30:35 +0500

    Update: views and templates fixed

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,47 @@
 from django.shortcuts import render
-from .models import Post
-
+from .models import Post, MeningIsmim, MeningFamilyam, University, Yoshim
 
 def home(request):
-    return render(request, 'home.html')
-
+        postlar = Post.objects.all()
+        context = {'postlar': postlar}
+        return render(request, 'home.html', context)
 
 def mening_ismim(request):
-    return render(request, 'mening_ismim.html')
+    ismlar = MeningIsmim.objects.all()
+    context = {
+        'title': 'Mening ismim',
+        'ismlar': ismlar
+    }
+    return render(request, 'mening_ismim.html', context)
 
 def mening_familyam(request):
-    return render(request, 'mening_familyam.html')
-
+    familyalar = MeningFamilyam.objects.all()
+    context = {
+        'title': 'Mening familyam',
+        'familyalar': familyalar
+    }
+    return render(request, 'mening_familyam.html', context)
 
 def yoshim(request):
-    return render(request, 'yoshim.html')
+    yoshlar = Yoshim.objects.all()
+    context = {
+        'title': 'Mening yoshim',
+        'yoshlar': yoshlar
+    }
+    return render(request, 'yoshim.html', context)
 
 def university(request):
-    return render(request, 'university.html')
+    universities = University.objects.all()
+    context = {
+        'title': 'Universitetim',
+        'universities': universities
+    }
+    return render(request, 'university.html', context)
 
 def postlar(request):
-    postlar = Post.objects.all()
+    postlar = Post.objects.all().order_by('-id')
     context = {
-        'postlar': postlar}
+        'title': 'Barcha postlar',
+        'postlar': postlar
+    }
     return render(request, 'postlar.html', context)
